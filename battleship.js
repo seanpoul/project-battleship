c4ceb17fccb16c1a5f838839e1ee5c4023faf96e
Sean <seanpoulter@hotmail.com>
2023-03-13 20:56:09 -0400

    put some test templates in test file

--- battleship.js
+++ battleship.js
@@ -1,3 +1,18 @@
-function ship(length, hit, sunk) {
+function ship(length) {
+    let ship = {
+        length: length,
+        hits: [],
+        isSunk: false
+    }
+    return ship
+}
+
+function hit() {
 
 }
+
+function isSunk(){
+
+}
+
+module.exports = ship;
---
+++ battleship.test.js
@@ -0,0 +1,21 @@
+const ship = require('./battleship');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+
+describe('calculator', () => {
+    test('add numbers', () => {
+        expect(calculator(1, "+", 1)).toBe(2)
+    })
+    test('subtract numbers', () => {
+        expect(calculator(2, "-", 1)).toBe(1)
+    })
+    test('multiply numbers', () => {
+        expect(calculator(4, "*", 2)).toBe(8)
+    })
+    test('divide numbers', () => {
+        expect(calculator(6, "/", 3)).toBe(2)
+    })
+});
