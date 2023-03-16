d21b0ae551fb55d9ebc02a2098dd97e226c92973
Sean <seanpoulter@hotmail.com>
2023-03-16 19:54:43 -0400

    create game board with minimal styles

--- src/board.js
+++ src/board.js
@@ -3,9 +3,11 @@ let board = document.getElementById("board");
 (function gameBoard() {
     for (let i = 0; i < 10; i++) {
         let row = document.createElement("div");
+        row.className = "row"
         board.appendChild(row);
         for (let j = 0; j < 10; j++) {
             let column = document.createElement("div");
+            column.className = "column";
             column.innerHTML = "H";
             row.appendChild(column);
         }
