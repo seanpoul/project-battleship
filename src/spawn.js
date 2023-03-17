0c9e5ceeedf9663b7c6d4bcc7a6bf28384e68c5b
Sean <seanpoulter@hotmail.com>
2023-03-16 23:11:59 -0400

    need to add webpack properly

--- src/spawn.js
+++ src/spawn.js
@@ -1,1 +1,15 @@
-let ship = [];
+import {ship} from "./battleship";
+
+let spawnShip = [];
+
+let pos1 = Math.floor(Math.random()*10);
+let po2 = pos1 + ship.length;
+console.log(pos1);
+
+console.log("yes")
+// console.log(ship.length)
+
+function place() {
+
+
+}
