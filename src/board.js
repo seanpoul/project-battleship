11e44e993a3cf3fbaa0107fe8a8cf098745c46e3
Sean <seanpoulter@hotmail.com>
2023-03-16 21:32:56 -0400

    add play and computer classes to each board

--- src/board.js
+++ src/board.js
@@ -1,16 +1,20 @@
 let playerBoard = document.getElementById("playerBoard");
 let computerBoard = document.getElementById("computerBoard");
 
+playerBoard.className = "player";
+computerBoard.className = "computer";
+
 function gameBoard(board) {
     for (let i = 0; i < 10; i++) {
-        let row = document.createElement("div");
-        row.className = "row"
-        board.appendChild(row);
+        let column = document.createElement("div");
+        column.classList.add("column" + i);
+        board.appendChild(column);
         for (let j = 0; j < 10; j++) {
-            let column = document.createElement("div");
-            column.className = "column";
-            column.innerHTML = "X";
-            row.appendChild(column);
+            let row = document.createElement("div");
+            row.classList.add(board.id);
+            row.classList.add("row" + j);
+            row.innerHTML = "X";
+            column.appendChild(row);
         }
     }
 }
