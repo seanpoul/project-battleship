534b552986a3e32b56aba02e53677f311d4c58a8
Sean <seanpoulter@hotmail.com>
2023-03-19 19:35:17 -0400

    add watch func. to package.json

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ let pos1 = Math.floor(Math.random()*10);
 let po2 = pos1 + ship.length;
 console.log(pos1);
 
-console.log("yes")
+// console.log("yes")
 // console.log(ship.length)
 
 function place() {
