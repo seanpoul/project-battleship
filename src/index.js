8d66116d3b8ca91eb1adf62f7c4fec63b6c90f12
Sean <seanpoulter@hotmail.com>
2023-03-19 21:39:56 -0400

    ship class added and 5 ships made

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ enemyBoard.className = "enemy";
 function gameBoard(board) {
     for (let i = 0; i < 10; i++) {
         let column = document.createElement("div");
-        // column.classList.add("column" + i);
         board.appendChild(column);
         for (let j = 0; j < 10; j++) {
             let row = document.createElement("div");
-            row.classList.add(board.id);
+            // row.classList.add(board.id);
             row.classList.add("column" + i);
             row.classList.add("row" + j);
             row.innerHTML = "X";
             column.appendChild(row);
         }
+    }
+    if (board == enemyBoard) {
+        handleClick()
     }
 }
 
@@ -24,21 +26,32 @@ gameBoard(playerBoard)
 gameBoard(enemyBoard)
 
 
-let clicked = document.getElementById("enemyBoard");
+function handleClick() {
+    let clicked = document.getElementById("enemyBoard");
 
-clicked.addEventListener("click", function (event) {
-    console.log(event.target.classList)
-    hit(event)
-})
-
-function ship(length) {
-    let ship = {
-        length: length,
-        hits: [],
-        isSunk: false
-    }
-    return ship
+    clicked.addEventListener("click", function (event) {
+        console.log(event.target.classList)
+        hit(event)
+    })
 }
+
+
+class Ship {
+    constructor(lengths) {
+        this.length = lengths;
+        this.hits = [];
+        this.isSunk = false;
+    }
+}
+
+let i = 0;
+
+let shipOne = new Ship(1);
+let shipTwo = new Ship(2);
+let shipThree = new Ship(3);
+let shipFour = new Ship(4);
+let shipFive = new Ship(5);
+
 
 function hit(event) {
     event.target.style.backgroundColor = "red";
@@ -54,9 +67,9 @@ function isSunk() {
 
 let spawnShip = [];
 
-let pos1 = Math.floor(Math.random()*10);
-let po2 = pos1 + ship.length;
-console.log(pos1);
+let pos1 = Math.floor(Math.random() * 10);
+// let po2 = pos1 + ship.length;
+// console.log(pos1);
 
 // console.log("yes")
 // console.log(ship.length)
